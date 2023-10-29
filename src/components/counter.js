d73145e39523b3031b53f29ca4d094df9fac099e
azhan3 <96319134+azhan3@users.noreply.github.com>
2023-10-28 22:23:37 -0400

    Stop counter from recounting every time the class enters view

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import CountUp from "react-countup";
 import VisibilitySensor from 'react-visibility-sensor';
 
-const Counter = props => (
-  <div className='flex-col flex items-center m-8'>
+const Counter = props => {
+    const [hasCounted, setHasCounted] = useState(false);
 
-    <CountUp start={0} end={props.number} redraw={true} >
-        {({ countUpRef, start }) => (
-            <VisibilitySensor onChange={start} delayedCall>
-                <span ref={countUpRef} className='text-7xl w-96 text-center'/>
-            </VisibilitySensor>
-        )}
-    </CountUp>
-    <h1 className='text-3xl font-bold mt-1 w-84 text-center'>{props.title}</h1>
-  </div>
-);
+    return (
+        <div className='flex-col flex items-center m-8'>
+            <CountUp start={0} end={hasCounted ? props.number : 0} redraw={true}>
+                {({ countUpRef, start }) => (
+                    <VisibilitySensor
+                        partialVisibility={true}
+                        onChange={isVisible => {
+                            if (isVisible && !hasCounted) {
+                                start();
+                                setHasCounted(true);
+                            }
+                        }}
+                        delayedCall
+                    >
+                        <span ref={countUpRef} className='text-7xl w-96 text-center' />
+                    </VisibilitySensor>
+                )}
+            </CountUp>
+            <h1 className='text-3xl font-bold mt-1 w-84 text-center'>{props.title}</h1>
+        </div>
+    );
+};
 
 export default Counter;
